7b16bab09745ec02169d57aa3fd7340c03f6e511
Tony.Liang <pillar0514@163.com>
2017-03-20 17:21:39 +0800

    添加日期选择器

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # spore-ui-picker
 各种picker
+
+## Release History
+
+ * 2017-03-20 v0.1.0 发布第一个正式版
--- src/date.js
+++ src/date.js
@@ -2,12 +2,21 @@
 @see https://github.com/chmln/flatpickr
 **/
 
+require('flatpickr/dist/flatpickr.css');
+
 var $flatpickr = require('flatpickr');
+var $locale = require('flatpickr/dist/l10n/zh');
+
+$flatpickr.l10ns.zh = $locale.zh;
 
 module.exports = function(options) {
 	var conf = $.extend({
-
+		node: null,
+		locale: $flatpickr.l10ns.zh
 	}, options);
 
-	return new $flatpickr(conf);
+	var node = conf.node;
+	delete conf.node;
+
+	return new $flatpickr(node, conf);
 };
